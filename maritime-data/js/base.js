ef2693a91b387da9d7a5dc8bf2e43a80244575de
vincezhou <vincezhou77@gmail.com>
2019-03-13 16:03:55 +0800

    add number

--- maritime-data/js/base.js
+++ maritime-data/js/base.js
@@ -1,4 +1,23 @@
 $(function(){
-
-    
+$('.data-rescue .module-info .soujiu .huan').animate({ backgroundPositionY:2},1500);
+$('.goods-box .info .icon-yuan .sep').eq(0).animate({ height:190},2000);
+$('.goods-box .info .icon-yuan .sep').eq(1).animate({ height:80},2000);
+$('.goods-box .info .icon-yuan .sep').eq(2).animate({ height:40},2000);
+$('.goods-box .info .icon-yuan .sep').eq(3).animate({ height:25},2000);
+$('.goods-box .info .icon-yuan .icon').eq(0).animate({ bottom:235},2000);
+$('.goods-box .info .icon-yuan .icon').eq(1).animate({ bottom:125},2000);
+$('.goods-box .info .icon-yuan .icon').eq(2).animate({ bottom:85},2000);
+$('.goods-box .info .icon-yuan .icon').eq(3).animate({ bottom:70},2000);
+$("#dataNums-01").rollNumDaq({
+  deVal:6712,
+  className:'dataNums'
+});
+$("#dataNums-02").rollNumDaq({
+  deVal:261,
+  className:'dataNums02'
+});
+$("#dataNums-03").rollNumDaq({
+  deVal:13,
+  className:'dataNums03'
+});
 });
