9d3d3d35f9a7009fef55ea3b5a00d0bf405827a1
vincezhou <vincezhou77@gmail.com>
2019-03-25 18:28:26 +0800

    crew 完成

--- crew/js/base.js
+++ crew/js/base.js
@@ -1,5 +1,12 @@
 $(function(){
 
 
-
 });
+!$('.inp-date').length > 0 || (function () {
+lay('.inp-date').each(function(){
+  laydate.render({
+    elem: this
+    ,trigger: 'click'
+  });
+}); 
+})();
