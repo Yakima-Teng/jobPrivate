b092f408ca16adfb3517993b83d83fd95c702488
vincezhou <vincezhou77@gmail.com>
2019-04-11 15:58:56 +0800

    crew修改

--- crew/js/base.js
+++ crew/js/base.js
@@ -1,7 +1,3 @@
-$(function(){
-
-
-});
 !$('.inp-date').length > 0 || (function () {
 lay('.inp-date').each(function(){
   laydate.render({
@@ -9,4 +5,21 @@ lay('.inp-date').each(function(){
     ,trigger: 'click'
   });
 }); 
+})();
+
+!$('.btn-position-tab').length > 0 || (function () {
+$('.hot-serch').find('a').on('click', function(){
+  var aTxt = $(this).text();
+  $('.index-content').find('.inp-txt').val(aTxt);
+});
+$('.btn-position-tab').find('.current').on('click',function(){
+  $('.current').addClass('cur');
+  $('.select').show();
+});
+$('.select').find('.xuanze').on('click',function(){
+  var curTxt = $(this).text();
+  $('.btn-position-tab').find('.current').text(curTxt).removeClass('cur');
+  $('.select').hide();
+});
+
 })();
