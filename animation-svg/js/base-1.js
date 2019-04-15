9b6e04b581266c1319e18c4c95c5ff8dc15a60ee
vincezhou <vincezhou77@gmail.com>
2019-04-15 14:53:45 +0800

    modify animation-svg

---
+++ animation-svg/js/base-1.js
@@ -0,0 +1,28 @@
+$('.page-index').toggle(function(){
+  $('.coordpath-yellow').eq(0).addClass('coordpath-yellow-01');
+  $('.coordpath-yellow').eq(1).addClass('coordpath-yellow-02');
+},function(){
+  $('.coordpath-yellow').eq(2).addClass('coordpath-yellow-03');
+  $('.coordpath-yellow').eq(3).addClass('coordpath-yellow-04');
+  $('.coordpath-yellow').eq(4).addClass('coordpath-yellow-05');
+  $('.coordpath-yellow').eq(5).addClass('coordpath-yellow-06');
+},function(){
+  $('.coordpath-yellow').eq(6).addClass('coordpath-yellow-07');
+  $('.coordpath-yellow').eq(7).addClass('coordpath-yellow-08');
+},function(){
+  $('.coordpath-red').eq(0).addClass('coordpath-red-01');
+},function(){
+  $('.coordpath-red').eq(1).addClass('coordpath-red-02');
+  $('.coordpath-red').eq(2).addClass('coordpath-red-03');
+},function(){
+  $('.coordpath-violet').eq(0).addClass('coordpath-violet-01');
+},function(){
+  $('.coordpath-blue').eq(0).addClass('coordpath-blue-01');
+},function(){
+  $('.coordpath-blue').eq(1).addClass('coordpath-blue-02');
+},function(){
+  $('.coordpath-blue').eq(2).addClass('coordpath-blue-03');
+},function(){
+  location.reload();
+});
+
