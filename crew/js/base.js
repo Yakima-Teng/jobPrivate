b6d352cdfb2f4ee2aa4f635451ee9a3458f26b7e
vincezhou <vincezhou77@gmail.com>
2019-04-15 10:32:49 +0800

    modify crew

--- crew/js/base.js
+++ crew/js/base.js
@@ -25,3 +25,80 @@ $('.select').find('.xuanze').on('click',function(){
 });
 
 })();
+
+!$('.police-confirm').length > 0 || (function () {
+$('.police-confirm').on('click',function(){
+    var allframe = $(this).parents('.crew-detailed-info');
+    allframe.find('.crew-info-img').show();
+    allframe.find('.crew-info-basis').hide();
+});
+$('.icon-coordinate').on('click',function(){
+    //$(this).next('.coordinate').show();
+});
+})();
+
+!$('#container-radar').length > 0 || (function () {
+var domRadar = document.getElementById("container-radar");
+var myChartRadar = echarts.init(domRadar);
+var appRadar = {};
+option = null;
+option = {
+radar: {
+radius: 80,
+name: {
+    textStyle: {
+        color: '#141414',
+        borderRadius: 3,
+        padding: [-12, -10]
+    }
+},
+indicator: [
+    { name: '年龄', max: 6500},
+    { name: '航龄', max: 52000},
+    { name: '航程', max: 38000},
+    { name: '记分', max: 30000},
+    { name: '晋级速率', max: 16000}
+],
+splitArea: {
+    areaStyle: {
+        color: ['rgba(0, 103, 244, 0.95)',
+        'rgba(0, 103, 244, 0.75)', 'rgba(0, 103, 244, 0.45)',
+        'rgba(0, 103, 244, 0.30)', 'rgba(0, 103, 244, 0.15)']
+    }
+},
+axisLine: {
+    lineStyle: {
+        color: 'rgba(0, 103, 244, 0.18)'
+    }
+},
+splitLine: {
+    lineStyle: {
+        color: 'rgba(255, 255, 255, 0)'
+    }
+}
+},
+series: [{
+type: 'radar',
+data : [
+    {
+        value : [4300, 10000, 28000, 3500, 5000],
+        symbolSize: 0,
+        areaStyle: {
+            normal: {
+                color: 'rgba(0, 103, 244, 0.42)'
+            }
+        },
+        lineStyle: {
+            normal: {
+                type: 'solid',
+                color:'rgba(0, 103, 244, 0.80)'
+            }
+        }
+    }
+]
+}]
+};;
+if (option && typeof option === "object") {
+    myChartRadar.setOption(option, true);
+}
+})();
