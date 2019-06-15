eeada3a3f92598cd587a417f87634fd76cd08772
vincezhou <vincezhou77@gmail.com>
2019-06-15 15:29:11 +0800

    modified data-background

--- data-background/js/base.js
+++ data-background/js/base.js
@@ -1,11 +1,15 @@
 $(function(){
-$('.page-contents').css({'paddingLeft':90});
+//$('.page-contents').css({'paddingLeft':90});
 $('.btn-shrink').toggle(function(){
   $(this).parent().removeClass('shrink');
-  $('.page-contents').css({'paddingLeft':255});
+//  $('.page-contents').css({'paddingLeft':255});
+  $('.page-header .slogan').css({'marginLeft':255+35});
+  $('.contents-box').css({'paddingLeft':255});
 },function(){
   $(this).parent().addClass('shrink');
-  $('.page-contents').css({'paddingLeft':90});
+ // $('.page-contents').css({'paddingLeft':90});
+ $('.page-header .slogan').css({'marginLeft':90+35});
+ $('.contents-box').css({'paddingLeft':90});
 })
 
 });
