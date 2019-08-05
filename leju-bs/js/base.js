47949a9b91f72522afdd41f9d1b382a76817fda0
vincezhou <vincezhou77@gmail.com>
2019-08-05 19:42:25 +0800

    add leju-bs

---
+++ leju-bs/js/base.js
@@ -0,0 +1,17 @@
+$(function(){
+  var mySwiper = new Swiper('.swiper-container',{
+
+    loop:true,
+    grabCursor: true,
+    paginationClickable: true
+  })
+  $('.arrow-left').on('click', function(e){
+    e.preventDefault()
+    mySwiper.swipePrev()
+  })
+  $('.arrow-right').on('click', function(e){
+    e.preventDefault()
+    mySwiper.swipeNext()
+  })
+    
+});
